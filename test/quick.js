4804ed748c1fec4afd5a1f5c29b8d73f7f946561
JosePerigolo <neto.perigolo@gmail.com>
2024-04-19 00:31:54 -0300

    Add quick test file

--- test/quick.js
+++ test/quick.js
@@ -1,61 +1,40 @@
-require('dotenv').config({ path: '.env.local' })
-// console.log(process.env) // remove this
+require('dotenv').config({ path: '.env.local' });
 
-const Seneca = require('seneca')
-
-run()
+const Seneca = require('seneca');
+const VectraStore = require('./path/to/VectraStore'); // Adjust the path as necessary
 
 async function run() {
   const seneca = Seneca({ legacy: false })
-    .test()
+    .test() // use .test() for test mode which suppresses logs except errors
     .use('promisify')
     .use('entity')
-    .use('..', {
-      map: {
-        'foo/chunk': '*',
-      },
-      index: {
-        exact: process.env.SENECA_OPENSEARCH_TEST_INDEX,
-      },
-      opensearch: {
-        node: process.env.SENECA_OPENSEARCH_TEST_NODE,
-      },
-    })
+    .use(VectraStore, {
+      vectra: {
+        path: process.env.VECTRA_DB_PATH, // Specify the path to Vectra DB
+      }
+    });
 
-  await seneca.ready()
+  await seneca.ready();
 
-  // console.log(await seneca.entity('bar/qaz').data$({q:1}).save$())
+  // Example data to save and then load
+  const exampleData = {
+    text: 'Hello Vectra',
+    vector: [0.1, 0.2, 0.3, 0.4, 0.5]
+  };
 
-  /*
-  const save0 = await seneca.entity('foo/chunk')
-        .make$()
-        .data$({
-          x:3,
-          o:{m:'M2',n:3}, 
-          text: 't03',
-          vector: [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.6],
-          directive$:{vector$:true},
-        })
-        .save$()
-  console.log('save0', save0)
-  */
+  // Saving data
+  const savedEntity = await seneca.entity('foo/item')
+    .make$(exampleData)
+    .save$();
+  console.log('Saved:', savedEntity);
 
-  // const id = '1%3A0%3Au0rACY4BB33NxQZdwDrQ'
-  // const id = 'notanid'
-  //const id = '1%3A0%3AvUrfCY4BB33NxQZd-DrZ'
-  const id = '1%3A0%3AvUrfCY4BB33NxQZd-DrQ'
-  const load0 = await seneca.entity('foo/chunk').load$(id)
-  console.log('load0', load0)
+  // Loading data using the ID from saved data
+  const loadedEntity = await seneca.entity('foo/item').load$(savedEntity.id);
+  console.log('Loaded:', loadedEntity);
 
-  /*
-  const list0 = await seneca.entity('foo/chunk').list$({
-    // x:2
-    directive$:{vector$:true},
-    vector:[0.1,0.1,0.2,0.3,0.4,0.5,0.6,0.7],
-  })
-  console.log('list0', list0)
-
-
-  console.log(await seneca.entity('bar/qaz').list$())
-  */
+  // Optionally, list all items
+  const items = await seneca.entity('foo/item').list$();
+  console.log('All items:', items);
 }
+
+run().catch(err => console.error(err));
